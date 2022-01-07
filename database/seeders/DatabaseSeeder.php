c74d741ef0ad5cc384f3c334d9b2d492e7a096db
KhaepahGithub1 <khayffahhepah@gmail.com>
2022-01-07 13:05:48 +0700

    first init

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,72 +29,114 @@ class DatabaseSeeder extends Seeder
         //Buah
         Buah::create([
             'nama' => 'Buah Apel',
-            'jenis_vitamin' => 'A',
+            'jenis_vitamin' => 'Vitamin A',
             'gambar' => 'buah/buahapel.jpg',
             'status' => 'Buah',
         ]);
 
         Buah::create([
             'nama' => 'Buah Alpokat',
-            'jenis_vitamin' => 'C',
+            'jenis_vitamin' => 'Vitamin C',
             'gambar' => 'buah/buahalpokat.jpg',
             'status' => 'Buah',
         ]);
 
         Buah::create([
             'nama' => 'Buah Mangga',
-            'jenis_vitamin' => 'C',
+            'jenis_vitamin' => 'Vitamin C',
             'gambar' => 'buah/buahmangga.jpg',
             'status' => 'Buah',
         ]);
 
         Buah::create([
             'nama' => 'Buah Naga',
-            'jenis_vitamin' => 'C',
+            'jenis_vitamin' => 'Vitamin C',
             'gambar' => 'buah/buahnaga.jpg',
             'status' => 'Buah',
         ]);
 
         Buah::create([
             'nama' => 'Buah Jeruk',
-            'jenis_vitamin' => 'C',
+            'jenis_vitamin' => 'Vitamin C',
             'gambar' => 'buah/buahjeruk.jpg',
             'status' => 'Buah',
         ]);
 
         Buah::create([
             'nama' => 'Buah Pisang',
-            'jenis_vitamin' => 'C',
+            'jenis_vitamin' => 'Vitamin C',
             'gambar' => 'buah/buahpisang.jpg',
             'status' => 'Buah',
         ]);
 
-        Sayur::create([
+        Buah::create([
             'nama' => 'Sayur Bayam',
             'jenis_vitamin' => 'A, B, C dan E',
             'gambar' => 'sayur/sayurbayam.jpg',
             'status' => 'Sayur',
         ]);
 
-        Sayur::create([
+        Buah::create([
             'nama' => 'Sayur Kol',
-            'jenis_vitamin' => 'C',
+            'jenis_vitamin' => 'Vitamin C',
             'gambar' => 'sayur/sayurkol.jpg',
             'status' => 'Sayur',
         ]);
 
-        Sayur::create([
+        Buah::create([
             'nama' => 'Sayur Tomat',
-            'jenis_vitamin' => 'C',
+            'jenis_vitamin' => 'Vitamin C',
             'gambar' => 'sayur/sayurtomat.jpg',
             'status' => 'Sayur',
         ]);
 
-        Sayur::create([
+        Buah::create([
             'nama' => 'Sayur wortel',
             'jenis_vitamin' => 'A, K1 dan B6',
             'gambar' => 'sayur/sayurwortel.jpg',
             'status' => 'Sayur',
+        ]);
+
+        Sayur::create([
+            'nama' => 'Ikan Salmon',
+            'jenis_vitamin' => 'Vitamin B dan D',
+            'gambar' => 'sayur/ikansalmon.jpg',
+            'status' => 'Ikan',
+        ]);
+
+        Sayur::create([
+            'nama' => 'Telur',
+            'jenis_vitamin' => '1 telur 6 gram protein',
+            'gambar' => 'sayur/telur.jpg',
+            'status' => 'Telur',
+        ]);
+
+        Sayur::create([
+            'nama' => 'Daging Sapi',
+            'jenis_vitamin' => '50 gram protein.',
+            'gambar' => 'sayur/daging.jpg',
+            'status' => 'Daging',
+        ]);
+
+        Sayur::create([
+            'nama' => 'Daging Ayam',
+            'jenis_vitamin' => '50 gram protein.',
+            'gambar' => 'sayur/dagingayam.jpg',
+            'status' => 'Daging',
+        ]);
+
+        Sayur::create([
+            'nama' => 'Susu',
+            'jenis_vitamin' => 'vitamin B2 dan B6',
+            'gambar' => 'sayur/susu.jpg',
+            'status' => 'Susu',
+        ]);
+
+        Sayur::create([
+            'nama' => 'Kacang',
+            'jenis_vitamin' => 'vitamin B',
+            'gambar' => 'sayur/kacang.jpg',
+            'status' => 'Kacang',
         ]);
     }
 }
